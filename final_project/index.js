6ff3783b79efd46a0517b2e57812168a46a743a5
Jose Aspra <jbaspra@gmail.com>
2023-07-24 19:56:35 -0400

    last updates 2023-07-24 07:56pm

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,8 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+let books = []
+
 const app = express();
 
 app.use(express.json());
